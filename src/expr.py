8354a8f842a1a2c67800a0f1c93900128354011c
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-07-06 11:57:55 +0700

    Grammar for Lox expressions

---
+++ src/expr.py
@@ -0,0 +1,59 @@
+class Expr:
+
+    def accept(self):
+        return NotImplemented()
+
+
+class ExprVisitor:
+
+    def visit_literal_expr(self, expr: "Literal"):
+        pass
+
+    def visit_unary_expr(self, expr: "Unary"):
+        pass
+
+    def visit_binary_expr(self, expr: "Binary"):
+        pass
+
+    def visit_grouping_expr(self, expr: "Grouping"):
+        pass
+
+
+class Literal(Expr):
+
+    def __init__(self, value):
+        self.value = value
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_literal_expr(self)
+
+
+class Grouping(Expr):
+
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_grouping_expr(self)
+
+
+
+class Unary(Expr):
+
+    def __init__(self, operator, right):
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_unary_expr(self)
+
+
+class Binary(Expr):
+
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_binary_expr(self)
