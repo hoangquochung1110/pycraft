f594ca2eab3901927abe8b30c0c5ad75042f4df5
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-09-18 10:54:09 +0700

    Statements and State: Environment Scope

--- src/pycraft/environment.py
+++ src/pycraft/environment.py
@@ -5,20 +5,30 @@ from .exception import LoxRuntimeError
 class Environment:
     values = dict()
 
+    def __init__(self, enclosing: "Environment"=None):
+        self.enclosing = enclosing
+
     def define(self, name, value):
         self.values[name] = value
 
     def get(self, name: Token):
+        # TODO: iteratively walk the chain instead for much faster
         if name.lexeme in self.values:
             return self.values[name.lexeme]
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
         raise LoxRuntimeError(
             name,
             "Undefined variable '" + name.lexeme + "'.",
         )
 
     def assign(self, name: Token, value):
+        # TODO: iteratively walk the chain instead for much faster
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
+            return
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
             return
         raise LoxRuntimeError(
             name,
